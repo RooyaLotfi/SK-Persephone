2d94eb6e7d6d2b421f02979e937f34aac9d0a5c7
Roya Lotfi <roya.lotfi@airms-iMac-Pro.localdomain>
2021-01-21 11:41:59 -0600

    updated resample to get target crs

--- Utils/resample.py
+++ Utils/resample.py
@@ -1,7 +1,9 @@
 import os
+import rioxarray as rxr
 
 
-def resample(path_in, in_crs, path_out, out_crs='EPSG:32613', target_resolution=30, resampling_method='bilinear'):
+def resample(path_in, path_out, out_crs='EPSG:32613', target_resolution=30, resampling_method='bilinear'):
+    in_crs = rxr.open_rasterio(path_in).rio.crs
     command = f"gdalwarp -s_srs {in_crs} -t_srs {out_crs} -tr {target_resolution}.0 {target_resolution}.0 -r {resampling_method} -tap -of GTiff {path_in} {path_out}"
     output = os.system(command)
     print(output)
