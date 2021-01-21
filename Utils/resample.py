594dcce46de3f019a66dd719343c1375596d12b3
Roya Lotfi <roya.lotfi@airms-iMac-Pro.localdomain>
2021-01-21 09:40:51 -0600

    updated resample method to get input and output crs

--- Utils/resample.py
+++ Utils/resample.py
@@ -1,6 +1,7 @@
 import os
 
-def resample(path_in, path_out, target_resolution, resampling_method):
-    command = f"gdalwarp -s_srs EPSG:4326 -t_srs EPSG:32613 -tr {target_resolution}.0 {target_resolution}.0 -r {resampling_method} -tap -of GTiff {path_in} {path_out}"
+
+def resample(path_in, in_crs, path_out, out_crs='EPSG:32613', target_resolution=30, resampling_method='bilinear'):
+    command = f"gdalwarp -s_srs {in_crs} -t_srs {out_crs} -tr {target_resolution}.0 {target_resolution}.0 -r {resampling_method} -tap -of GTiff {path_in} {path_out}"
     output = os.system(command)
     print(output)
